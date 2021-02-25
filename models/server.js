dc7209d21a98a8260a137936181a9d951b3245dc
Marc Viladegut <mv8@alumnes.udl.cat>
2021-02-25 16:08:30 +0100

    Broadcast communication between server and client

--- models/server.js
+++ models/server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const cors = require('cors');
+const { socketController } = require('../sockets/controller');
 
 class Server {
 
     constructor() {
         this.app  = express();
         this.port = process.env.PORT;
+        this.server = require('http').createServer(this.app);
+        this.io = require('socket.io')(this.server);
 
         this.paths = {}
 
@@ -14,6 +17,9 @@ class Server {
 
         // Aplication routes
         this.routes();
+
+        // Socket configuration
+        this.sockets()
     }
 
     middlewares() {
@@ -32,8 +38,12 @@ class Server {
     
     }
 
+    sockets(){
+        this.io.on("connection", socketController)
+    }
+
     listen() {
-        this.app.listen( this.port, () => {
+        this.server.listen( this.port, () => {
             console.log('Servidor corriendo en puerto', this.port );
         });
     }
--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -1,2 +1,40 @@
 
-console.log('Hello World!')
+// Html references
+const lblOnline = document.querySelector('#lblOnline')
+const lblOffline = document.querySelector('#lblOffline')
+const txtMensaje = document.querySelector('#txtMensaje')
+const btnEnviar = document.querySelector('#btnEnviar')
+
+
+const socket = io()
+
+socket.on('connect', () => {
+    console.log('Client connected')
+    lblOffline.style.display = 'none'
+    lblOnline.style.display = ''
+})
+
+
+socket.on('disconnect', () => {
+    lblOffline.style.display = ''
+    lblOnline.style.display = 'none'
+})
+
+socket.on('send-message', (payload) => {
+    console.log(payload)
+})
+
+
+btnEnviar.addEventListener('click', () => {
+
+    const mensaje = txtMensaje.value
+    const payload = {
+        mensaje,
+        id: '123ABC',
+        fecha: new Date().getTime()
+    }
+    socket.emit('send-message', payload, (id) => {
+        console.log(id)
+    })
+
+})
