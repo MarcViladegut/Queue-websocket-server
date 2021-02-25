d723be910ee9f4c0a8b46b55710b8b6a86064511
Marc Viladegut <mv8@alumnes.udl.cat>
2021-02-25 12:44:56 +0100

    Initialize the services and the requirements to start the webserver

---
+++ models/server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const cors = require('cors');
+
+class Server {
+
+    constructor() {
+        this.app  = express();
+        this.port = process.env.PORT;
+
+        this.paths = {}
+
+        // Middlewares
+        this.middlewares();
+
+        // Aplication routes
+        this.routes();
+    }
+
+    middlewares() {
+
+        // CORS
+        this.app.use( cors() );
+
+        // Public directory
+        this.app.use( express.static('public') );
+
+    }
+
+    routes() {
+
+        //this.app.use( this.paths.auth, require('../routes/auth'));
+    
+    }
+
+    listen() {
+        this.app.listen( this.port, () => {
+            console.log('Servidor corriendo en puerto', this.port );
+        });
+    }
+
+}
+
+
+
+
+module.exports = Server;
